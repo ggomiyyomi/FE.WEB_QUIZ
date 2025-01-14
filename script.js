5fe9e7773e69e2835860ac8171a40df6b6c06de9
HeeJeongSeo <heejung9965@naver.com>
2025-01-14 21:25:41 +0900

    Quiz box -ing

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 
 startBtn.onclick = () => {
@@ -20,4 +21,96 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
+
+
+    showQuestions(0);
+    questionCounter(1);
+    headerScore();
+}
+
+let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if(questionCount < questions.length -1 ) {
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+
+        nextBtn.classList.remove('active');
+    }
+    else {
+        console.log('Question Completed');
+    }
+}
+
+const optionList = document.querySelector('.option-list');
+
+// 배열로부터 퀴즈와 보기 가져오기
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`
+
+
+    // 공백오류위험으로 잘 살펴보기
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for(let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)')
+    }
+}
+
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer === correctAnswer) {
+        answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
+    }
+    else {
+        answer.classList.add('incorrect');
+
+
+        //만약 답이 틀렸다면, 자동으로 정답 선택되도록 해야함
+        for( let i =0; i < allOptions; i++){
+            if(optionList.children[i].textContent == correctAnswer) {
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+        }
+    }
+
+    // 만약 유저가 선택했을 경우, 모든 옵션을 비활성화시켜야함
+    for( let i =0; i < allOptions; i++){
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+}
+
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+
+function headerScore() {
+    const headerScoreText = document.querySelector('.header-score');
+    headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
 }
